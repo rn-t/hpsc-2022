3e8b598bcdc837c5e5618cc47c541ecc0cc06430
Raku Numata <numata.r.aa@m.titech.ac.jp>
2022-05-22 07:48:29 +0900

    16の操作ののち0, 250万, 500万, 750万が合計値として出力されたのでおそらく完成。

--- 10_io/17_phdf5_read.cpp
+++ 10_io/17_phdf5_read.cpp
@@ -6,12 +6,14 @@
 using namespace std;
 
 int main (int argc, char** argv) {
-  hsize_t dim[2] = {2, 2};
+  //hsize_t dim[2] = {2, 2};
+  hsize_t dim[2] = {4, 4};
   int mpisize, mpirank;
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
-  assert(mpisize == dim[0]*dim[1]);
+  //assert(mpisize == dim[0]*dim[1]);
+  assert(mpisize == dim[0]*dim[1] / 4);
   hid_t plist = H5Pcreate(H5P_FILE_ACCESS);
   H5Pset_fapl_mpio(plist, MPI_COMM_WORLD, MPI_INFO_NULL);
   hid_t file = H5Fopen("data.h5", H5F_ACC_RDONLY, plist);
@@ -22,14 +24,22 @@ int main (int argc, char** argv) {
   H5Sget_simple_extent_dims(globalspace, N, NULL);
   hsize_t NX = N[0], NY = N[1];
   hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
-  hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
+  //hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
+  hsize_t offset[2] = {mpirank / (dim[0] / 2), mpirank % (dim[0] / 2)};
   for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
-  hsize_t count[2] = {1,1};
-  hsize_t stride[2] = {1,1};
-  hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
+  //hsize_t count[2] = {1,1};
+  hsize_t count[2] = {2,2};
+  //hsize_t stride[2] = {1,1};
+  hsize_t stride[2] = {2,2};
+  for(int i=0; i<2; i++) stride[i] *= Nlocal[i];
+  
+  hsize_t Nlocal_2[2] = {Nlocal[0] * 2,Nlocal[1] * 2};
+
+  //hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
+  hid_t localspace = H5Screate_simple(2, Nlocal_2, NULL);
   H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, Nlocal);
   H5Pclose(plist);
-  vector<int> buffer(Nlocal[0]*Nlocal[1]);
+  vector<int> buffer(Nlocal[0]*Nlocal[1] * 4);
   plist = H5Pcreate(H5P_DATASET_XFER);
   H5Pset_dxpl_mpio(plist, H5FD_MPIO_COLLECTIVE);
   auto tic = chrono::steady_clock::now();
@@ -42,7 +52,8 @@ int main (int argc, char** argv) {
   H5Pclose(plist);
   double time = chrono::duration<double>(toc - tic).count();
   int sum = 0;
-  for (int i=0; i<Nlocal[0]*Nlocal[1]; i++)
+  //for (int i=0; i<Nlocal[0]*Nlocal[1]; i++)
+  for (int i=0; i<Nlocal[0]*Nlocal[1] * 4; i++)
     sum += buffer[i];
   printf("sum=%d\n",sum);
   printf("N=%d: %lf s (%lf GB/s)\n",NX*NY,time,4*NX*NY/time/1e9);
